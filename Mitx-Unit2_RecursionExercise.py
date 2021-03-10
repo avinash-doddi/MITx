4d92d9041678eedb52e4e341f9c65e5a2972919c
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-11 00:22:37 +0530

    Update Mitx-Unit2_RecursionExercise.py

--- Mitx-Unit2_RecursionExercise.py
+++ Mitx-Unit2_RecursionExercise.py
@@ -26,9 +26,43 @@ def iterPower(base, exp):
  """ Well, this is quite understandable"""
 
 print(iterPower(int(input("base : ")),int(input('exp : '))))
+
+
+___________________________________________________________________________________________________________________________________________________________________
    
    
- __________________________________________________________________________________________________________________________________________________________________
+
+
+"""In Problem 1, we computed an exponential by iteratively executing successive multiplications. We can use the same idea, but in a recursive function.
+
+Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same problem,
+and then multiplying the result by base to solve the initial problem.
+
+This function should take in two values - base can be a float or an integer; exp will be an integer ≥0. It should return one numerical value. 
+Your code must be recursive - use of the ** operator or looping constructs is not allowed.
+Code Editor"""
+
+
+def recurPower(base, exp):
+
+    '''
+    base: int or float.
+    
+    exp: int >= 0
+    
+    returns: int or float, base^exp
+    '''
+    # Your code here
+
+    if exp > 0:
+       return base*recurPower(base, exp-1)
+    else : 
+       return 1
+     
+print(recurPower(int(input("base : ")), int(input("exp : ")))
+      
+      
+___________________________________________________________________________________________________________________________________________________________________
  
  
  """fibonacci Series Using Recursive Function"""
